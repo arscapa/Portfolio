b8a3cfdfd0daa75b7da48227a67a24be07f265a3
Tony Scapardine <arscapa@gmail.com>
2015-06-28 20:21:52 -0500

    Improved readability of app.js file by eliminating individual $(document).ready code and wrapping entire file in one $(document).ready

--- Online_Resume/app.js
+++ Online_Resume/app.js
@@ -1,156 +1,154 @@
 ﻿/// <reference path="C:\Users\HawksFan\Documents\Visual Studio 2013\Projects\Online_Resume\Online_Resume\Scripts/jwplayer/jwplayer.js" />
 
-//programmatically adds active class to Active navigation page
-var str = location.href.toLowerCase();
-$("#nav li a").each(function () {
-    if (str.indexOf($(this).attr("href").toLowerCase()) > -1) {
-        $("li.current-menu-item").removeClass("current-menu-item");
-        $(this).parent().addClass("current-menu-item");
-    }
-});
-$("li.current-menu-item").parents().each(function () {
-    if ($(this).is("li")) {
-        $(this).addClass("current-menu-item");
-    }
-});
-
-
-
-// Zoom animation for about me image links on default page
-var AboutMeIMGAnimate = function () {
-    $('div.About_Me_Links a img').mouseover(function () {
-
-        $(this).animate({
-            height: '+=10px',
-            width: '+=10px',
-        })
-    });
-
-
-    $('div.About_Me_Links a img').mouseout(function () {
-
-        $(this).animate({
-            height: '-=10px',
-            width: '-=10px',
-        })
-    });
-}
-$(document).ready(AboutMeIMGAnimate)
-
-
-
-
-//loads lightbox when clicking corresponding image map hotspot on interests page
-
-
 $(document).ready(function () {
+
+    //programmatically adds active class to Active navigation page
+    var str = location.href.toLowerCase();
+    $("#nav li a").each(function () {
+        if (str.indexOf($(this).attr("href").toLowerCase()) > -1) {
+            $("li.current-menu-item").removeClass("current-menu-item");
+            $(this).parent().addClass("current-menu-item");
+        }
+    });
+    $("li.current-menu-item").parents().each(function () {
+        if ($(this).is("li")) {
+            $(this).addClass("current-menu-item");
+        }
+    });
+
+
+
+    // Zoom animation for about me image links on default page
+    var AboutMeIMGAnimate = function () {
+        $('div.About_Me_Links a img').mouseover(function () {
+
+            $(this).animate({
+                height: '+=10px',
+                width: '+=10px',
+            })
+        });
+
+
+        $('div.About_Me_Links a img').mouseout(function () {
+
+            $(this).animate({
+                height: '-=10px',
+                width: '-=10px',
+            })
+        });
+    }
+    $(document).ready(AboutMeIMGAnimate)
+
+
+
+
+    //loads lightbox when clicking corresponding image map hotspot on interests page
+
+
+
     $('area.SelectSuspension').click(function () {
         $("#span1").click();
     });
 
-    
-        $('area.SelectEngine').click(function () {
-            $("#span2").click();
-        });
+
+    $('area.SelectEngine').click(function () {
+        $("#span2").click();
+    });
 
 
-        $('area.SelectBody').click(function () {
-            $("#span3").click();
-        });
+    $('area.SelectBody').click(function () {
+        $("#span3").click();
+    });
 
 
-        $('area.SelectInterior').click(function () {
-            $("#span4").click();
-        });
-});
-
-//preload charger_main images so that they're ready for hover event
+    $('area.SelectInterior').click(function () {
+        $("#span4").click();
+    });
 
 
-function preloader() 
-
-{
+    //preload charger_main images so that they're ready for hover event
 
 
-    // counter
-    var i = 0;
+    function preloader() {
 
 
-    // create object
-    imageObj = new Image();
+        // counter
+        var i = 0;
 
 
-    // set image list
-    images = new Array();
-    images[0] = "Pictures/AutomotiveResto/MainPics/74dodgeEngine.png"
-    images[1] = "Pictures/AutomotiveResto/MainPics/74dodgeSuspension.png"
-    images[2] = "Pictures/AutomotiveResto/MainPics/74dodgeFender.png"
-    images[3] = "Pictures/AutomotiveResto/MainPics/74dodgeInterior.png"
+        // create object
+        imageObj = new Image();
 
 
-    // start preloading
-    for(i=0; i<=3; i++) 
-    {
-        imageObj.src=images[i];
+        // set image list
+        images = new Array();
+        images[0] = "Pictures/AutomotiveResto/MainPics/74dodgeEngine.png"
+        images[1] = "Pictures/AutomotiveResto/MainPics/74dodgeSuspension.png"
+        images[2] = "Pictures/AutomotiveResto/MainPics/74dodgeFender.png"
+        images[3] = "Pictures/AutomotiveResto/MainPics/74dodgeInterior.png"
+
+
+        // start preloading
+        for (i = 0; i <= 3; i++) {
+            imageObj.src = images[i];
+        }
+
     }
 
-} 
 
 
 
 
+    //changes charger_main picture on interests page to highlighted section of car when hovering over it
 
-//changes charger_main picture on interests page to highlighted section of car when hovering over it
-
-$(document).ready(function () {
     $('area.SelectEngine').hover(function () {
         $("#Charger_Main").attr('src', 'Pictures/AutomotiveResto/MainPics/74dodgeEngine.png');
-   
-        })
+
+    })
 
     $('area.SelectSuspension').hover(function () {
         $("#Charger_Main").attr('src', 'Pictures/AutomotiveResto/MainPics/74dodgeSuspension.png');
-   
-           })
+
+    })
 
     $('area.SelectBody').hover(function () {
         $("#Charger_Main").attr('src', 'Pictures/AutomotiveResto/MainPics/74dodgeFender.png');
-   
-            })
+
+    })
 
     $('area.SelectInterior').hover(function () {
         $("#Charger_Main").attr('src', 'Pictures/AutomotiveResto/MainPics/74dodgeInterior.png');
-  
+
 
         $('#Charger_Main').mouseout(function () {
-            $("#Charger_Main").attr('src','Pictures/AutomotiveResto/MainPics/74dodge.png')
+            $("#Charger_Main").attr('src', 'Pictures/AutomotiveResto/MainPics/74dodge.png')
         })
-            })
-
-});
-
-
-//load fancybox
-$(".fancybox").click(function() {
-    $(".fancybox").fancybox({
-        type: "iframe",
     });
-});
 
 
 
-//image map resizer--> changes coordinates of image map when window is resized
 
-$(document).ready(function () {
+    //load fancybox
+    $(".fancybox").click(function () {
+        $(".fancybox").fancybox({
+            type: "iframe",
+        });
+    });
+
+
+
+    //image map resizer--> changes coordinates of image map when window is resized
+
+
     $('map').imageMapResize();
-});
 
 
 
 
 
 
-//scrollmagic functionality--changes background color when scrolling over Charger image
+
+    //scrollmagic functionality--changes background color when scrolling over Charger image
 
 
     // Init Controller
@@ -159,8 +157,8 @@ $(document).ready(function () {
     // Create Animation for 1s
     var tween = new TimelineMax()
 .add([
-    
-     
+
+
         TweenMax.to('.bold', 1, { color: '#3333FF', fontSize: "20px" }),
 
 ]);
@@ -170,20 +168,20 @@ $(document).ready(function () {
     var scene = new ScrollMagic.Scene({
         triggerElement: '#trigger1', // starting scene, when reaching this element
         offset: 200,
-        
+
     })
 
-    
+
 
 // Add Scene to ScrollMagic Controller
 .setTween(tween)
 .addTo(controller)
-scene.reverse(true)  //change reverse option to false after adding to controller
-.addIndicators()
+    scene.reverse(true)  //change reverse option to false after adding to controller
+    .addIndicators()
 
 
 
-//scrollmagic--change background back to normal
+    //scrollmagic--change background back to normal
 
 
     // Init Controller
@@ -193,9 +191,9 @@ scene.reverse(true)  //change reverse option to false after adding to controller
     var tween2 = new TimelineMax()
 .add([
 
-    
+
         TweenMax.to('.bold', .8, { color: 'black', fontSize: "17px" }),
-    
+
 
 ]);
 
@@ -208,11 +206,11 @@ scene.reverse(true)  //change reverse option to false after adding to controller
 // Add Scene to ScrollMagic Controller
 .setTween(tween2)
 .addTo(controller2)
-scene2.reverse(true)
+    scene2.reverse(true)
 
 
 
-//scrollmagic functionality for skill bar
+    //scrollmagic functionality for skill bar
 
 
     // Init Controller
@@ -241,51 +239,54 @@ scene2.reverse(true)
 .addTo(controller3)
 
 
-//collapse expand accordian
+    //collapse expand accordian
 
-    $(document).ready(function () {
-        function close_accordion_section() {
-            $('.accordion .accordion-section-title').removeClass('active');
-            $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title').click(function (e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if ($(e.target).is('.active')) {
+            close_accordion_section();
+        } else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
         }
 
-        $('.accordion-section-title').click(function (e) {
-            // Grab current anchor value
-            var currentAttrValue = $(this).attr('href');
-
-            if ($(e.target).is('.active')) {
-                close_accordion_section();
-            } else {
-                close_accordion_section();
-
-                // Add active class to section title
-                $(this).addClass('active');
-                // Open up the hidden content panel
-                $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
-            }
-
-            e.preventDefault();
-        });
+        e.preventDefault();
     });
 
 
-// Set interval for logo Carousel
+
+    // Set interval for logo Carousel
 
     $('.accordion #flag3').slideDown(function () {
         $('#logoCarousel').carousel({
             interval: 2500
         })
     });
-   
-  
-//Open corresponding accordion section when link clicked from home page, (checks url for tag and opens corresponding section)
 
-var str = location.href.toLowerCase();
-$(".accordion-section-title").each(function () {
-    if (str.indexOf($(this).attr("href").toLowerCase()) > -1) {
-        $('.accordion .accordion-section-title').removeClass('active');
-        $('.accordion .accordion-section-content').slideUp(800).removeClass('open');
-        $(this).addClass("active");
-        $(this).next('.accordion-section-content').slideDown(800).addClass("open")
-    }
+
+    //Open corresponding accordion section when link clicked from home page, (checks url for tag and opens corresponding section)
+
+    var str = location.href.toLowerCase();
+    $(".accordion-section-title").each(function () {
+        if (str.indexOf($(this).attr("href").toLowerCase()) > -1) {
+            $('.accordion .accordion-section-title').removeClass('active');
+            $('.accordion .accordion-section-content').slideUp(800).removeClass('open');
+            $(this).addClass("active");
+            $(this).next('.accordion-section-content').slideDown(800).addClass("open")
+        }
+    });
+
+
 });
