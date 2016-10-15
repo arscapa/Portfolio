eac4e3b5d2d4e191e0c6d9bcfea081cad7f2ef72
Tony Scapardine <arscapa@gmail.com>
2016-10-15 16:43:43 -0500

    Added javascript code to make skill-bar animation functional again as it was accidently deleted in last commit. Also, added EXCELDEMO video to project and to gitignore list. This video was removed from prior commit as file exceeded 100mb GitHub limit.

--- Online_Resume/app.js
+++ Online_Resume/app.js
@@ -144,6 +144,33 @@ $(document).ready(function () {
 
 
 
+    //scrollmagic functionality for skill bar
+    
+    
+        // Init Controller
+        var controller3 = new ScrollMagic.Controller();
+    
+        // Create Animation for 0.5s
+        var tween3 = new TimelineMax()
+    .add([
+    
+            TweenMax.to('.skill_bar1', 1.25, { width: '90%', }),
+            TweenMax.to('.skill_bar2', 1.25, { width: '80%', }),
+            TweenMax.to('.skill_bar3', 1.25, { width: '80%', }),
+            TweenMax.to('.skill_bar4', 1.25, { width: '65%', }),
+            TweenMax.to('.skill_bar5', 1.25, { width: '65%', }),
+    ]);
+    
+        //Create Scene
+        var scene3 = new ScrollMagic.Scene({
+                triggerElement: '#trigger3', // starting scene, when reaching this element
+                offset: -450,
+            })
+ 
+ 
+ // Add Scene to ScrollMagic Controller
+ .setTween(tween3)
+ .addTo(controller3)
 
 
 
